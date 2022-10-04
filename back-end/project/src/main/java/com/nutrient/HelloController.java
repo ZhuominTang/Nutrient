418e10f1be34862d62aea826d5315c30b54167ef
ZhuominTang <tang.zhuo@northeastern.edu>
2022-10-03 22:21:24 -0400

    update

---
+++ back-end/project/src/main/java/com/nutrient/HelloController.java
@@ -0,0 +1,17 @@
+package com.nutrient;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World!";
+    }
+
+
+}
