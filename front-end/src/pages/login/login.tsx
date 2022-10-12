b588daba17d254fcf50c6d744e2ff88c9bb419aa
ZhuominTang <tang.zhuo@northeastern.edu>
2022-10-12 15:14:55 -0400

    update

--- front-end/src/pages/login/login.tsx
+++ front-end/src/pages/login/login.tsx
@@ -1,14 +1,27 @@
 import React,{FC} from 'react';
+import imgURL from '../../assets/images/logo-neg.png';
+import './login.scss';
+
 
 
 const Login: FC = () => {
 
 
   return (
-    <>
+ 
 
-     <div>login</div>
-    </>
+<div className="login">
+				<header className="login-header">
+					<img src={imgURL} alt="logo"></img>
+					<h1>Nutrition Information Management system</h1>
+				</header>
+				<section className="login-content">
+					
+					<h2>User Login</h2>
+				
+				</section>
+			</div>
+  
 
 
   )
