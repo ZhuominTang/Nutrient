a4341873ea72876ea6114b30f0e5d9062d026eb6
ZhuominTang <tang.zhuo@northeastern.edu>
2022-10-12 15:29:42 -0400

    update

--- front-end/src/pages/login/login.tsx
+++ front-end/src/pages/login/login.tsx
@@ -12,8 +12,10 @@ const Login: FC = () => {
 
 <div className="login">
 				<header className="login-header">
+                    <div className='logo'>
 					<img src={imgURL} alt="logo"></img>
 					<h1>Nutrition Information Management system</h1>
+                    </div>
 				</header>
 				<section className="login-content">
 					
