fdafd5d1fa8c85f00c49d8dc63ccf89514a67601
ZhuominTang <tang.zhuo@northeastern.edu>
2022-10-13 16:22:17 -0400

    update login page

--- front-end/src/pages/login/registerform/registerForm.tsx
+++ front-end/src/pages/login/registerform/registerForm.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { useState }  from 'react'
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './registerForm.scss';
 
 const RegisterForm = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+      const form = e.currentTarget;
+      if (form.checkValidity() === false) {
+        e.preventDefault();
+        e.stopPropagation();
+      }
+  
+      setValidated(true);
+    };
   return (
-    <div>registerForm</div>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="Email" placeholder="Enter Email" required />       
+        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="validationCustom01">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Username"
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        aria-describedby="passwordHelpBlock"
+        placeholder="Password"
+        required
+      />
+      <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
+      <Button className="submitButton" variant="primary" type="submit">
+        Register
+      </Button>
+    </Form>
   )
 }
 export default RegisterForm;
