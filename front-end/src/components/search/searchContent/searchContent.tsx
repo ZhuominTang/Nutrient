85b9e2cfc0d3bcc2ffe0dce1824fae4c932d03de
ZhuominTang <tang.zhuo@northeastern.edu>
2022-11-03 13:40:55 -0400

    update

--- front-end/src/components/search/searchContent/searchContent.tsx
+++ front-end/src/components/search/searchContent/searchContent.tsx
@@ -1,36 +1,36 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 import SearchMessage from './searchDetail/searchMessage';
 import { useEffect } from 'react';
 
 
 
-interface Props{
+interface Props {
     keyword: string
 }
 
 
-const SearchContent = (props:Props) => {
-    const [message,setMessage] = useState({
-        
-            keyword: props.keyword,
-            pageNo: 0,
-            pageSize: 10
-        
-    });
-    useEffect(()=>{
-        let newMessage = Object.assign({},message,{keyword:props.keyword,pageNo:0})
-        setMessage(newMessage)
-    },[props.keyword])
+const SearchContent = (props: Props) => {
+    const [message, setMessage] = useState({
 
-    const getMessage = (pageNumber : number) =>{
-        let newMessage = Object.assign({},message,{pageNo:(pageNumber-1)*10})
+        keyword: props.keyword,
+        pageNo: 0,
+        pageSize: 10
+
+    });
+    useEffect(() => {
+        let newMessage = Object.assign({}, message, { keyword: props.keyword, pageNo: 0 })
+        setMessage(newMessage)
+    }, [props.keyword])
+
+    const getMessage = (pageNumber: number) => {
+        let newMessage = Object.assign({}, message, { pageNo: (pageNumber - 1) * 10 })
         setMessage(newMessage)
     }
 
     return (
         <>
-                <SearchMessage getMessage={getMessage} data={message}></SearchMessage>
+            <SearchMessage getMessage={getMessage} data={message}></SearchMessage>
         </>
 
     )
